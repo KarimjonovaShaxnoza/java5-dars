51c5658aa9e259201c93de55bac9d8a74047872e
KarimjonovaShaxnoza <shaxnozakarimjonova100@gmail.com>
2024-09-19 16:28:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting: ")
+let id = prompt("Space ID kiriting: ")
+let password = +prompt("Parol kiriting: ")
+
+let person = {
+    firstName: ism,
+    id: id,
+    password: password
+}
+
+console.log(person);
